6022fe28681ec67ad198a4cf09231707037cac59
dvdsk <noreply@davidsk.dev>
2022-03-24 21:37:49 +0100

    adds copy_pasta for handeling dutch linkedin req.

--- keyboards/ergodox_ez/keymaps/dvdsk/dvdsk.h
+++ keyboards/ergodox_ez/keymaps/dvdsk/dvdsk.h
@@ -11,11 +11,27 @@
 #define RELEASE(keycode) unregister_code16(keycode)
 #define PREVENT_STUCK_MODIFIERS
 
+const char* linkedin_nl_yes = "Beste <name>,\n"
+"\n"
+"Leuk! Ik ben nu vrij ver in een sollicitatie voor een functie die 6k pm oplevert. Indien jullie een voor mij competitief salaris bieden maak ik graag tijd voor je vrij. En anders wens ik je veel succes met je zoektocht.\n"
+"\n"
+"Vriendelijke groet,\n"
+"David Kleingeld\n"
+"http://davidsk.dev/about";
+
+const char* linkedin_nl_no = ""
+"Ik vrees dat deze functie niet bij mij past. Ik wens je veel succes met het vinden van geschikte kandidaten.\n"
+"\n"
+"Vriendelijke groet,\n"
+"David Kleingeld\n"
+"http://davidsk.dev/about";
 
 #define CLOSE_WIN_INTERVAL 300
 enum macroKeycodes {
   M_BAT = SAFE_RANGE, //begin alt tab
   M_EAT,              //end alt-tab
+  M_LIN_NLY,
+  M_LIN_NLN,
   MAIL_END,
   WIN_MM,
   TAPP,
